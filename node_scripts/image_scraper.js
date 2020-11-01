f7d0d8c5a5771ef76a7d4dedcd1768cd0e6318d3
Y Paul Sussman <ypaulsussman@gmail.com>
2020-11-01 14:17:55 -0600

    add pdf image extractor

---
+++ node_scripts/image_scraper.js
@@ -0,0 +1,52 @@
+import fs from "fs";
+import path from "path";
+import { exec } from "child_process";
+
+// $ node ~Desktop/convenience_scripts/node_scripts/image_scraper.js ~/Desktop/path/to/toplevel/dir/
+// NB need to mkdir first
+
+const extractImages = (file, fullPath) => {
+  exec(
+    `pdfimages -all "${fullPath}" ~/Desktop/testerpics/${file.replace(/ /g, "\\ ").slice(0, -4)}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    }
+  );
+};
+
+const walk = function (directoryName) {
+  fs.readdir(directoryName, function (e, files) {
+    if (e) {
+      console.log("readdir error: ", e);
+      return;
+    }
+    files.forEach(function (file) {
+      var fullPath = path.join(directoryName, file);
+      fs.stat(fullPath, function (e, f) {
+        if (e) {
+          console.log("stat error: ", e);
+          return;
+        }
+        if (f.isDirectory()) {
+          walk(fullPath);
+        } else {
+          console.log("file: ", file);
+          console.log("fullpath: ", fullPath);
+          extractImages(file, fullPath);
+        }
+      });
+    });
+  });
+};
+
+const startingDir = process.argv[2];
+console.log("startingDir: ", startingDir);
+walk(startingDir);
